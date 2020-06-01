33d1b03dea0c400b045fff4afde5910b42cfbac1
Natalie Tan <natalie.tan@rea-group.com>
2020-06-01 17:31:40 +1000

    Upgrade gradle wrapper, build tools, dependencies
    - Upgrade gradle wrapper from 3.3 to 6.4.1
    - Upgrade gradle build tools from 2.3 to 4.0
    - Add dependencies and plugins for Kotlin
    - Move to androidX
    - target SDK version 29

--- app/src/main/java/au/com/realestate/hometime/MainActivity.java
+++ app/src/main/java/au/com/realestate/hometime/MainActivity.java
@@ -2,7 +2,7 @@ package au.com.realestate.hometime;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
