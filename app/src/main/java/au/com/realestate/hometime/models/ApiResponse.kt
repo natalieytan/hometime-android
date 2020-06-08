c630524f576a257e0df8b4c04a67676b0ad924b3
Natalie Tan <natalie.tan@rea-group.com>
2020-06-08 14:58:59 +1000

    Remove ApiResponse fields that we ignore

--- app/src/main/java/au/com/realestate/hometime/models/ApiResponse.kt
+++ app/src/main/java/au/com/realestate/hometime/models/ApiResponse.kt
@@ -1,8 +1,5 @@
 package au.com.realestate.hometime.models
 
 data class ApiResponse<T>(
-    val errorMessage: String? = null,
-    val hasError: Boolean? = null,
-    val hasResponse: Boolean? = null,
-    val responseObject: List<T>? = null
+    val responseObject: List<T>
 )
